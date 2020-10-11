04a0231ed735b5632f2a8d20e55bec1a147478c8
Valeria Mustetsova <lera.mustetsova57@gmail.com>
2020-10-11 12:20:57 +0300

    "Add project"

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+          useBuiltIns: 'usage',
+      },
+    ],
+  ],
+  plugins: [
+    "@babel/plugin-proposal-class-properties"
+  ],
+};
+
